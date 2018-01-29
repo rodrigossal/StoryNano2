748362831e127c5e1e7e1413f3534c0451d390f5
Rodrigo Salles Stefani <rodrigo.salless@hotmail.com>
2018-01-29 17:09:04 -0200

    começo de tuuuudo

---
+++ Story/Story/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by Rodrigo Salles Stefani on 29/01/18.
+//  Copyright © 2018 Rodrigo Salles Stefani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
